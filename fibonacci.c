ce2f2d361d61185b9455d2dc7a5c862d13e16600
majkel78 <michaljarz78@gmail.com>
2017-12-03 02:19:20 +0100

    Poprawki dla liczy 1

--- fibonacci.c
+++ fibonacci.c
@@ -5,23 +5,31 @@ int main() {
     unsigned long int liczba_kolejna;
     unsigned long int licznik;
     unsigned long int wynik = 0;
-    unsigned long int poprzednia1=0;
-    unsigned long int poprzednia2=1;
+    unsigned long int poprzednia1=0;	// zmienne do przechowywania poprzednich liczb w kolejnosci:
+    unsigned long int poprzednia2=1;	// poprzednia1, poprzednia2, liczba_kolejna
 
     printf("Podaj wartosc calkowita dla ktorej mam obliczyc liczbe Fibonacciego, n = ");
     scanf(" %ld", &liczba_kolejna);
 
-    if (liczba_kolejna <= 92) {
-
-        for (licznik = 1; licznik < liczba_kolejna; licznik++) {
-            wynik = poprzednia1+poprzednia2;
-            poprzednia1=poprzednia2;
-            poprzednia2=wynik;
+    if (liczba_kolejna <= 92)
+    {
+		if(liczba_kolejna<2)
+			wynik=liczba_kolejna;
+		else
+		{
+			for (licznik = 2; licznik <= liczba_kolejna; licznik++)
+			{
+				wynik = poprzednia1+poprzednia2;
+				poprzednia1=poprzednia2;			// poprzednia1=liczba_kolejna(-2) bedzie tym czym byla liczba_kolejna(-1)
+				poprzednia2=wynik;					// poprzednia2=liczba_kolejna(-1) bedzie tym czym byla liczba_kolejna(0)
+			}
         }
 
         printf("\nLiczba Fibonacciego (%ld): %ld\n", liczba_kolejna, wynik);
 
-    } else {
+    }
+    else
+    {
         printf("Za duza wartosc n, n <= 92\n");
     }
 
